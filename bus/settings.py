53bf05ce418315f71bc5fc65dfbd9d592e8be9df
AHEALTH <2670211238@qq.com>
2021-03-16 16:10:04 +0800

    '数据库推送'

--- bus/settings.py
+++ bus/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'bus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '',  # 数据库用户密码
+        'NAME': 'bus'  # 数据库名字
+
     }
 }
 
